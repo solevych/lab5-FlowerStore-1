deea4c4e41de0e3abfeede9a846845006c834f37
Bazylevych Sofiia <bazylevych.pn@ucu.edu.ua>
2024-10-13 20:28:50 +0300

    delete unnecessary file

--- src/test/java/flower/store/TypesTest.java
+++ src/test/java/flower/store/TypesTest.java
@@ -4,11 +4,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
-public class TypesTest {
-    private Rose rose;
-    private Tulip tulip;
-    private Chamomile chamomile;
+/**
+ * Tests for flower types including Rose, Tulip, and Chamomile.
+ */
 
+public class TypesTest {
+
+    private Rose rose;      
+    private Tulip tulip;   
+    private Chamomile chamomile; 
+
+    /**
+     * Initializes the flower instances before each test.
+     */
     @BeforeEach
     public void init() {
         rose = new Rose();
@@ -16,19 +24,25 @@ public class TypesTest {
         chamomile = new Chamomile();
     }
 
+    /**
+     * Tests the default flower type of Rose.
+     */
     @Test
     public void testRoseDefaultFlowerType() {
         Assertions.assertEquals(FlowerType.Rose, rose.getFlowerType());
     }
 
+    // Similar test for Tulip
     @Test
     public void testTulipDefaultFlowerType() {
         Assertions.assertEquals(FlowerType.Tulip, tulip.getFlowerType());
     }
 
+    // Similar test for Chamomile
     @Test
     public void testChamomileDefaultFlowerType() {
         Assertions.assertEquals(FlowerType.Chamomile, chamomile.getFlowerType());
     }
+
 
 }
